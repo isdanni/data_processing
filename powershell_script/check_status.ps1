6c9e33709d686b74e1583ba91f5da70a3f12fff4
Danni <38550500+isdanni@users.noreply.github.com>
2018-10-04 16:33:14 +0800

    Update check_status.ps1

--- powershell_script/check_status.ps1
+++ powershell_script/check_status.ps1
@@ -1,4 +1,4 @@
-$postParams = @{client_id='50fc1ee4af7c3cfbac7dd9b56f952fbb';client_secret='9b50cc37736d4576b64ece1bc7e78c55';grant_type='password';username='vesselit@oocl.com';password='Sea##856'}
+$postParams = @{client_id='*';client_secret='*';grant_type='password';username='*';password='*'}
 $result = Invoke-WebRequest -Headers $headers -Uri https://api.ic.peplink.com/api/oauth2/token -Method POST -Body $postParams
 $json = $result.Content| ConvertFrom-Json
 $uri = "https://api.ic.peplink.com/rest/o/gYr0QD/g/3/d?device_count=0&has_status=false&access_token="+$json.access_token
@@ -17,7 +17,7 @@ foreach ($i in $groupDevice.data){
     $params | Add-Member -MemberType NoteProperty -Name device_name -Value $i.name
     $status += $params
 }
-if(!(Test-Path  "E:\VesselCloud\pepwave\current_$(Get-Date -Format ddMMyyyy).csv")){
+if(!(Test-Path  "E:\*\current_$(Get-Date -Format ddMMyyyy).csv")){
     $status | convertto-csv -notype | out-file -Append -Encoding utf8 -filepath E:\VesselCloud\pepwave\current_$(Get-Date -Format ddMMyyyy).csv
 }
 else{
